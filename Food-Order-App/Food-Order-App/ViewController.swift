da5b37dc9e3b30f252fc31bafac2ffe9f5442842
oemurlu <oemurlu@gmail.com>
2023-11-04 14:55:02 +0300

    Add Xcode project

---
+++ Food-Order-App/Food-Order-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food-Order-App
+//
+//  Created by Osman Emre Ömürlü on 4.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
